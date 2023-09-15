ec34f34ba37fc128c697336a424a7dfe1c03f66b
Stadnikov Rostislav <stadnikov2000@gmail.com>
2023-09-15 16:18:25 +0300

    Created handlers

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ import asyncio
 
 async def on_startup(dp):
 	print("Bot is running")
+
+
+if __name__ == "__main__":
+	from aiogram import executor
+	from handlers import dp
+
+	executor.start_polling(dp, on_startup=on_startup)
--- data/config.py
+++ data/config.py
@@ -3,10 +3,8 @@ from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 
-TOKEN = os.getenv("TO1KEN")
+TOKEN = os.getenv("TOKEN")
 
 if TOKEN == None:
 	print("Fatal error. Check your TOKEN at .env ")
 	quit()
-
-print(TOKEN)
