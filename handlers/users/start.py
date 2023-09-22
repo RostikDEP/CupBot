98fd8c130c93dfc164820c42a01da60cb5d81170
Stadnikov Rostislav <stadnikov2000@gmail.com>
2023-09-22 18:43:11 +0300

    1

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -5,3 +5,4 @@ from loader import quotes, database
 @dp.message_handler(text='/start')
 async def command_start(message: types.Message):
 	await message.answer(quotes.commands['start'])
+	await message.answer(quotes.text['check_user'])
